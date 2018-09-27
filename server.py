8608f196510c469544c78196a9de8c9d218c1fb1
Andi innoz <andneuma@posteo.de>
2018-09-27 14:39:18 +0200

    Add some gammu code

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import gammu
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
 
@@ -11,22 +13,49 @@ def auth_token_valid(token = None):
 def ressource_protected():
     return 'AUTH_TOKEN' in os.environ
 
+# Gammu stuff
+# Taken from https://gammu.readthedocs.io/en/latest/python/examples.html and adapted
+
+def send_sms(number, message_text):
+    try:
+        # Create object for talking with phone
+        state_machine = gammu.StateMachine()
+        state_machine.ReadConfig()
+
+        # Connect to the phone
+        state_machine.Init()
+
+        # Prepare message data
+        # We tell that we want to use first SMSC number stored in phone
+        message = {
+                'Text': text,
+                'SMSC': {'Location': 1},
+                'Number': number
+                }
+
+        # Actually send the message
+        state_machine.SendSMS(message)
+        return {"status": "Successfully send!"}
+    except Exception as e:
+        return {"error": "An error occured while sending SMS"}
 
 @app.route('/send_sms/')
-def send_sms():
+def sms_request():
     params = request.args.to_dict()
+    auth_token = params['auth_token']
+    phone_number = params['phone_number']
+    message_text = params['message_text']
 
     if not ressource_protected():
         return '{"error": "Service not setup entirely!"}', 403
-    elif not auth_token_valid(params['auth_token']):
+    elif not auth_token_valid(auth_token):
         return '{"error": "Authentication not successfull!"}', 403
     else:
-        try:
-            # send_sms()
-            # return None, 200
-            return 'Worked!'
-        except:
-            return {"error": "An error occured while sending SMS"}
+        return {"foo": auth_token, "phone_number": phone_number, "message_text": message_text}, 200
+        # status = send_sms(phone_number, message_text)
+        # return status, 200
 
 if __name__ == '__main__':
+    # Set to false if FLASK_ENV=production????
+    # Otherwise set to false or sth bad might happen...
     app.run(debug=True)
